7b6a27522db690403762423a49fd5c481524f9a4
Farhod <farhod@example.com>
2025-09-03 16:19:30 +0500

    admin.py

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "created_at")
+    search_fields = ("name",)
+    ordering = ("-created_at",)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "category", "price", "is_active", "created_at")
+    list_filter = ("is_active", "category")
+    search_fields = ("title", "description", "location")
+    ordering = ("-created_at",)
